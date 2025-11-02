e48238b159c0c05b5a03ce60148c3abcbd6a2aed
Disantni4ik <dimasic16252@gmail.com>
2025-11-03 00:38:14 +0200

    Add transform origin to loader styles for improved animation

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ let progress = 0;
 const bar = document.getElementById("loader");
 
 const interval = setInterval(() => {
+  console.log(progress)
   progress += 0.5;
   bar.style.width = progress + "px";
 
