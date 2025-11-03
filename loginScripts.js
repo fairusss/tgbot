94432abe4b6a95912d7e1bb1e05c396078883825
fairusss <vladrobot50@gmail.com>
2025-11-03 21:23:23 +0100

    Fix logging statement indentation in login button click handler

--- loginScripts.js
+++ loginScripts.js
@@ -2,6 +2,6 @@ const tg = window.Telegram.WebApp;
 const obj = document.getElementById("TEST");
 
 document.getElementById("loginbtn").addEventListener("click", async () => {
-  
+    console.log("pisun");
     tg.sendData(JSON.stringify({ action: "request_contact"}));
 });
